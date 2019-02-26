0c54ea789e3bd2a01e43c4ea610637206d38cb4d
Jon Bulica <jonbulica99@gmail.com>
2019-02-26 15:52:29 +0100

    fix BaseObject

--- objects/base.py
+++ objects/base.py
@@ -6,7 +6,9 @@ __version__ = 0.1
 
 
 class BaseObject(Persistent):
-    def __init__(self, name=self.__class__.__name__, version=__version__):
+    def __init__(self, name=None, version=__version__):
+        if not name:
+            name = self.__class__.__name__
         self.name = name
         self.version = version
         self.log = Logger(name).get()
