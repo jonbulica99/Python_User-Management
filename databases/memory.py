3a3231abedf4265e76158a2e961c74a1abc4ff0c
Jon Bulica <jonbulica99@gmail.com>
2019-03-05 23:40:14 +0100

    implemented in-memory database

---
+++ databases/memory.py
@@ -0,0 +1,20 @@
+from databases.sqlite import Sqlite
+
+__version__ = 0.1
+
+class Memory(Sqlite):
+    def __init__(self):
+        super().__init__(file="", version=__version__)
+
+    def check_if_enough_memory(self, warn_percent=80, crit_percent=95, default=True):
+        try:
+            import psutil
+            mem_percent = psutil.virtual_memory().get("percent")
+            if mem_percent > crit_percent:
+                self.log.crit("Not enough system memory left (%s percent is being used).", mem_percent)
+                del self
+            elif mem_percent > warn_percent:
+                self.log.warn("The OS is running out of memory. If the kernel kills this process, you data will disappear forever! (%s percent is being used).", mem_percent)
+        except ModuleNotFoundError as e:
+            self.log.warn("Module psutil not found. Cannot check system memory before creating memory DB.")
+        return default
