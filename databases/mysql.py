a5119d64c002944af275e136e3013ac9eec03df8
Jon Bulica <jonbulica99@gmail.com>
2019-03-05 23:46:37 +0100

    implement postgresql

--- databases/mysql.py
+++ databases/mysql.py
@@ -5,7 +5,7 @@ __version__ = 0.1
 class Mysql(SqlAlchemy):
     implementation = "{user}:{pwd}@{host}:{port}"
     def __init__(self, database, user, pwd, host, port=3306, version=__version__):
-        super().__init__(database=database, dialect="mysql", version=version)
+        super().__init__(database=database, version=version)
         self.user = user
         self.pwd = pwd
         self.host = host
---
+++ databases/postgresql.py
@@ -0,0 +1,7 @@
+from databases.mysql import Mysql
+
+__version__ = 0.1
+
+class Postgresql(Mysql):
+    def __init__(self, database, user, pwd, host, port=5432, version=__version__):
+        super().__init__(database, user, pwd, host, port=port, version=version)
