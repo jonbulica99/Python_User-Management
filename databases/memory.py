8e1ba562d0a0594b17295edcfc14ef119ad9b649
Jon Bulica <jonbulica99@gmail.com>
2019-04-01 11:25:50 +0200

    fix sqlite issues

--- databases/memory.py
+++ databases/memory.py
@@ -12,7 +12,7 @@ class Memory(Sqlite):
     def check_if_enough_memory(self, warn_percent=80, crit_percent=95, default=True):
         try:
             import psutil
-            mem_percent = psutil.virtual_memory().get("percent")
+            mem_percent = psutil.virtual_memory().percent
             if mem_percent > crit_percent:
                 self.log.crit(
                     "Not enough system memory left (%s percent is being used).", mem_percent)
@@ -20,7 +20,7 @@ class Memory(Sqlite):
             elif mem_percent > warn_percent:
                 self.log.warning(
                     "The OS is running out of memory. If the kernel kills this process, you data will disappear forever! (%s percent is being used).", mem_percent)
-        except ModuleNotFoundError as e:
+        except ImportError as e:
             self.log.warning(
                 "Module psutil not found. Cannot check system memory before creating memory DB.")
         return default
