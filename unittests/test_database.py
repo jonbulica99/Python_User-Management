86795d386775b40494a15a0d5bd73d5171151f05
Jon Bulica <jonbulica99@gmail.com>
2019-05-20 13:08:38 +0200

    fix database unit tests

--- unittests/test_database.py
+++ unittests/test_database.py
@@ -4,17 +4,19 @@ from databases.sql_alchemy import SqlAlchemy
 from helpers.db_manager import DatabaseManager, DbType
 from utils.config import Config
 from objects import *
+from flask import Flask
 
 
 class DatabaseTests(unittest.TestCase):
     main_config = Config()
     manager = DatabaseManager(main_config)
     database = None
+    app = Flask(__name__)
 
     def create_db(self):
         if not self.database:
             self.database = self.manager.create_database(db_type=DbType.MEMORY)
-            self.database.connect()
+            self.database.connect(self.app)
         return self.database
 
     def test_initialize(self):
@@ -22,21 +24,22 @@ class DatabaseTests(unittest.TestCase):
         self.assertIsInstance(db, SqlAlchemy)
 
     def test_schema(self):
-        db = self.create_db()
-        try:
-            db.create_schema(Base)
-        except Exception as e:
-            self.fail("Failed to create the DB schema: " + e)
+        with self.app.app_context():
+            db = self.create_db()
+            try:
+                db.create_schema()
+            except Exception as e:
+                self.fail("Failed to create the DB schema: " + e)
 
     def test_default_values(self):
-        db = self.create_db()
-
-        # TODO: warum brauchen wir dies?
-        self.test_schema()
-        try:
-            self.manager.insert_default_values(db)
-        except Exception as e:
-            self.fail("Failed to insert default values: " + e)
+        with self.app.app_context():
+            db = self.create_db()
+            # TODO: warum brauchen wir dies?
+            self.test_schema()
+            try:
+                self.manager.insert_default_values(db)
+            except Exception as e:
+                self.fail("Failed to insert default values: " + e)
 
  
 if __name__ == '__main__':
