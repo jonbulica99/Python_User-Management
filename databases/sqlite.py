11e6fc0b1cb7694d19795d5fd37f8f847519b34b
Jon Bulica <jonbulica99@gmail.com>
2019-05-21 09:42:55 +0200

    fix faulty implementation of sqlite

--- databases/sqlite.py
+++ databases/sqlite.py
@@ -3,6 +3,6 @@ from databases.sql_alchemy import SqlAlchemy
 __version__ = 0.1
 
 class Sqlite(SqlAlchemy):
-    implementation = "{database}"
+    implementation = ""
     def __init__(self, database, version=__version__, *args, **kwargs):
         super().__init__(database=database, dialect="sqlite", version=version, *args, **kwargs)
