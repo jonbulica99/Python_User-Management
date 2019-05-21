c9b2022ca036294de426a9df839a07a3d8eb7d53
Jon Bulica <jonbulica99@gmail.com>
2019-05-21 11:48:25 +0200

    renamed unittests to tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,6 @@ from utils.constants import os, ROOT_DIR
 
 loader = unittest.TestLoader()
 # run all tests in the unittests directory
-suite = loader.discover(os.path.join(ROOT_DIR, 'unittests'))
+suite = loader.discover(os.path.join(ROOT_DIR, 'tests'))
 runner = unittest.TextTestRunner()
 runner.run(suite)
